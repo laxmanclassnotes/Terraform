d8e62d8c831c64a1c1ad2f816150a562897f3cbf
laxman <laxman1849@gmail.com>
2023-03-25 16:33:27 +0530

    aa

---
+++ JOIP_TASKS/DAY-2-6SUBNETS/inputs.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  type        = string
+  default     = "ap-southeast-1"
+  description = "Region to create resources"
+}
+
+variable "ntier-vpc-range" {
+  type        = string
+  default     = "192.168.0.0/16"
+  description = "VPC Cidr Range"
+}
+
+variable "ntier-subnet-cidrs" {
+  type    = list(string)
+  default = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+}
+
+variable "ntier-subnet-azs" {
+  type    = list(string)
+  default = ["a", "b", "a", "b"]
+
+}
+
+variable "ntier-subnet-names" {
+  type    = list(string)
+  default = ["app1", "app2", "db1", "db2"]
+}
---
+++ JOIP_TASKS/DAY-2-6SUBNETS/main.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "ntier" {
+  cidr_block = var.ntier-vpc-range
+  tags = {
+    Name = "ntier"
+  }
+
+}
+
+resource "aws_subnet" "subnets" {
+  count             = length(var.ntier-subnet-cidrs)
+  cidr_block        = var.ntier-subnet-cidrs[count.index]
+  availability_zone = "${var.region}${var.ntier-subnet-azs[count.index]}"
+  vpc_id            = aws_vpc.ntier.id #implicit dependency
+  depends_on = [
+    aws_vpc.ntier
+  ]
+  tags = {
+    Name = var.ntier-subnet-names[count.index]
+  }
+
+}
