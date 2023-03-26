3baefc6ef0ef8d17e6f2d74d89c427791fe393d8
laxman <laxman1849@gmail.com>
2023-03-26 15:26:13 +0530

    added changes in rds

---
+++ JOIP_TASKS/DAY-3/Resource-Task1/main.tf
@@ -0,0 +1,15 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+resource "aws_db_instance" "default" {
+  allocated_storage   = 10
+  db_name             = "mydb"
+  engine              = "mysql"
+  engine_version      = "5.7"
+  instance_class      = "db.t3.micro"
+  username            = "laxmandb"
+  password            = random_password.password.result
+  skip_final_snapshot = true
+}
