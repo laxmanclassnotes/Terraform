fc624a81a0036b5205ef87977fb887bef17ee977
laxman <laxman1849@gmail.com>
2023-03-28 16:36:32 +0530

    Added lenth using count

---
+++ AWS_TERRAFORM/Practice1/inputs.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type        = string
+  default     = "ap-southeast-1"
+  description = "to default region"
+}
+variable "basic-cidr-range" {
+  type        = string
+  default     = "192.168.0.0/16"
+  description = "to default cidr-block"
+}
+
+variable "basic-subnet-azs" {
+  type        = list(string)
+  default     = ["a", "b", "a", "b", "a", "b"]
+  description = "subnet azs"
+}
+variable "basic-subnet-names" {
+  type    = list(string)
+  default = ["app1", "app2", "db1", "db2", "web1", "web2"]
+}
