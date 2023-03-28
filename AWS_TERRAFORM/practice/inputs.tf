07979b539f8fd85854b0975f81ddb6ad203f7e5f
laxman <laxman1849@gmail.com>
2023-03-28 15:26:23 +0530

    Added length changes

--- AWS_TERRAFORM/practice/inputs.tf
+++ AWS_TERRAFORM/practice/inputs.tf
@@ -9,16 +9,16 @@ variable "basic-cidr-range" {
   default     = "192.168.0.0/16"
   description = "to default cidr-block"
 }
-variable "basic-subnet-cidrs" {
-  type    = list(string)
-  default = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24", "192.168.5.0/24"]
-}
+# variable "basic-subnet-cidrs" {
+#   type    = list(string)
+#   default = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24", "192.168.5.0/24"]
+# }
 variable "basic-subnet-azs" {
   type        = list(string)
   default     = ["a", "b", "a", "b", "a", "b"]
   description = "subnet azs"
 }
-variable "basic-subnet-Names" {
+variable "basic-subnet-names" {
   type    = list(string)
   default = ["app1", "app2", "db1", "db2", "web1", "web2"]
 }
