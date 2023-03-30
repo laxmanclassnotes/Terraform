bed41921d541ded56b68b5e75ab68e7875701272
laxman <laxman1849@gmail.com>
2023-03-30 20:21:11 +0530

    Added s3 list

--- AWS_TERRAFORM/Practice1/inputs.tf
+++ AWS_TERRAFORM/Practice1/inputs.tf
@@ -1,20 +1,23 @@
 variable "region" {
   type        = string
   default     = "ap-southeast-1"
-  description = "to default region"
-}
-variable "basic-cidr-range" {
-  type        = string
-  default     = "192.168.0.0/16"
-  description = "to default cidr-block"
+  description = "Region to create resources"
 }
 
-variable "basic-subnet-azs" {
-  type        = list(string)
-  default     = ["a", "b", "a", "b", "a", "b"]
-  description = "subnet azs"
-}
-variable "basic-subnet-names" {
-  type    = list(string)
-  default = ["app1", "app2", "db1", "db2", "web1", "web2"]
+variable "ntier_vpc_info" {
+  type = object({
+    vpc_cidr        = string,
+    subnet_azs      = list(string),
+    subnet_names    = list(string),
+    private_subnets = list(string),
+    public_subnets  = list(string)
+  })
+  default = {
+    subnet_azs      = ["a", "b", "a", "b"]
+    subnet_names    = ["app1", "app2", "db1", "db2"]
+    vpc_cidr        = "192.168.0.0/16"
+    public_subnets  = []
+    private_subnets = ["app1", "app2", "db1", "db2"]
+  }
+
 }
--- aws/practice/values.tfvars
+++
@@ -1,2 +0,0 @@
-region             = "ap-southeast-1"
-basic-cidr-range   = "10.100.0.0/16"
