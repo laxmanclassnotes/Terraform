f5b5df7f0f212616b80268ba5eb606890e1a4327
laxman <laxman1849@gmail.com>
2023-03-30 16:35:57 +0530

    added cftask using path

---
+++ JOIP_TASKS/Day-5/Task1/main.tf
@@ -0,0 +1,4 @@
+resource "aws_cloudformation_stack" "cfterraform" {
+  name          = "terraformcf"
+  template_body = file("c:/users/laxma/TERRAFORM-NOTES/Terraform/CloudFormation/BASIC-TASKS-CF/activity2.json")
+}
