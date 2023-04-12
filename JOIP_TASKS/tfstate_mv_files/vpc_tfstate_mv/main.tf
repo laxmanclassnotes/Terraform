cc90b02e9c5780e5bd7cefff2b79c6646a25bdb4
laxman <laxman1849@gmail.com>
2023-04-12 16:52:38 +0530

    Added terraform state mv threesubnets

---
+++ JOIP_TASKS/tfstate_mv_files/vpc_tfstate_mv/main.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = "192.168.0.0/16"
+    tags = {
+      Name = "myvpc"
+    }
+}
+
+resource "aws_subnet" "web" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "192.168.1.0/24"
+}
+
+resource "aws_subnet" "app" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "192.168.2.0/24"
+}
+
+resource "aws_subnet" "db" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "192.168.4.0/24" 
+}
